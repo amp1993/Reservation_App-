f8f93e8c3a8a8b6eaa34a2eb1117edf5f94af065
Alexandra Colorado <alexandrapena19@gmail.com>
2024-02-05 13:14:22 -0500

    Update code for name
    Update name

--- models/customer.js
+++ models/customer.js
@@ -12,6 +12,14 @@ class Customer {
     this.lastName = lastName;
     this.phone = phone;
     this.notes = notes;
+    this.concatName = this.concatName.bind(this);
+  }
+
+//Concatenate first and last name. 
+
+ concatName(){
+    const fullName = `${this.firstName} ${this.lastName}`
+    return fullName
   }
 
   /** find all customers. */
@@ -67,17 +75,31 @@ class Customer {
         `INSERT INTO customers (first_name, last_name, phone, notes)
              VALUES ($1, $2, $3, $4)
              RETURNING id`,
-        [this.firstName, this.lastName, this.phone, this.notes]
+        [this.concatName, this.phone, this.notes]
       );
       this.id = result.rows[0].id;
     } else {
       await db.query(
         `UPDATE customers SET first_name=$1, last_name=$2, phone=$3, notes=$4
              WHERE id=$5`,
-        [this.firstName, this.lastName, this.phone, this.notes, this.id]
+        [this.concatName, this.phone, this.notes, this.id]
       );
     }
   }
 }
+
+
+//PART 2
+//Create method for saving reervations
+
+
+//PART 3
+//Add a quick search form to the bootstrap navigation bar to search for a customer by name.
+//You can either make a new route and template to show results, or you could probably make it work with the existing listing index route and template (as you’d prefer).
+
+//PART 4
+//Add a new route that finds our top 10 customers ordered by most reservations.
+
+//
 
 module.exports = Customer;
